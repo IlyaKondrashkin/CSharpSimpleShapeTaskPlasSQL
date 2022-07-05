c9bc2836b5b5761cae6989b5109cd7456e6d7179
Ilya Kondrashkin <i.l.y.a.k@mail.ru>
2022-07-05 21:46:23 +0300

    Tab characters are removed.

--- sql_task/fill_up.sql
+++ sql_task/fill_up.sql
@@ -1,10 +1,10 @@
 INSERT INTO Products (Name)
 VALUES ('Butter'),
        ('Bread'),
-	   ('Shirt'),
-	   ('Trousers'),
-	   ('Not categorised product'),
-	   ('Charlie Chaplin`s eatable boot')
+       ('Shirt'),
+       ('Trousers'),
+       ('Not categorised product'),
+       ('Charlie Chaplin`s eatable boot')
 
 INSERT INTO Categories (Name)
 VALUES ('Food'),
@@ -13,7 +13,7 @@ VALUES ('Food'),
 INSERT INTO ProductsCategories (ProductId, CategoryId)
 VALUES ((SELECT Id FROM Products WHERE Name LIKE('Butter')), (SELECT Id FROM Categories WHERE Name LIKE('Food'))),
        ((SELECT Id FROM Products WHERE Name LIKE('Bread')), (SELECT Id FROM Categories WHERE Name LIKE('Food'))),
-	   ((SELECT Id FROM Products WHERE Name LIKE('Shirt')), (SELECT Id FROM Categories WHERE Name LIKE('Clothes'))),
+       ((SELECT Id FROM Products WHERE Name LIKE('Shirt')), (SELECT Id FROM Categories WHERE Name LIKE('Clothes'))),
        ((SELECT Id FROM Products WHERE Name LIKE('Trousers')), (SELECT Id FROM Categories WHERE Name LIKE('Clothes'))),
-	   ((SELECT Id FROM Products WHERE Name LIKE('Charlie Chaplin`s eatable boot')), (SELECT Id FROM Categories WHERE Name LIKE('Food'))),
+       ((SELECT Id FROM Products WHERE Name LIKE('Charlie Chaplin`s eatable boot')), (SELECT Id FROM Categories WHERE Name LIKE('Food'))),
        ((SELECT Id FROM Products WHERE Name LIKE('Charlie Chaplin`s eatable boot')), (SELECT Id FROM Categories WHERE Name LIKE('Clothes')));
